dc457859de00a7759537fa8ba5f201fbccb31b0f
Alicia Laurinen <alicialaurinen@Alicias-MacBook-Pro.local>
2023-11-13 13:09:08 +0100

    Done lab 4

--- src/components/OneBurger.vue
+++ src/components/OneBurger.vue
@@ -1,68 +1,74 @@
 <template>
-    <div>
-      <span id="burger">
-                        <h3>
-                          {{ burger.name }}
-                        </h3>
-                        <img class="bilder" v-bind:src="burger.img"/>
-                        <dl>
-                            <dt>Can be ordered as:</dt>
-                            <dd>{{ burger.gluten }}</dd>
-                            <dd>{{burger.lactose}}</dd>
-                            <dd>Calories: {{ burger.kCal }}</dd>
-                        </dl>
-                        Choose amount:
-                       <button v-on:click="Increase(key)"> more </button> 
-                       {{amountOrdered}} 
-                       <button v-on:click="Decrease(key)"> less </button>
-                       <div>
-                       <button v-on:click="addBurger()">
-                        <img src="img/cart.jpeg" width="30" height="30">
-                       </button>
-                      </div>
-                    </span>
-       {{ burger.URL}}
-    </div>
-  </template>
+  <div>
+    <span id="burger">
+      <h3>
+        {{ burger.name }}
+      </h3>
+      <img class="bilder" v-bind:src="burger.img" />
+      <dl>
+        <dt>Can be ordered as:</dt>
+        <dd>{{ burger.gluten }}</dd>
+        <dd>{{ burger.lactose }}</dd>
+        <div>
+
+        
+        Amount of Calories: {{ burger.kCal }}
+      </div>
+      </dl>
+      Choose amount:
+      <button v-on:click="Increase(key)"> more </button>
+      {{ amountOrdered }}
+      <button v-on:click="Decrease(key)"> less </button>
+      <div>
+        <button v-on:click="addBurger()">
+          <img src="img/cart.jpeg" width="30" height="30">
+        </button>
+      </div>
+    </span>
+    {{ burger.URL }}
+  </div>
+</template>
   
-  <script>
-  export default {
-    name: 'OneBurger',
-    props: {
-      burger: Object
+<script>
+export default {
+  name: 'OneBurger',
+  props: {
+    burger: Object
+  },
+  data: function () {
+    return {
+      amountOrdered: 0,
+    }
+  },
+  methods: {
+    Increase: function () {
+
+      this.amountOrdered += 1;
     },
-    data: function () {
-  return {
-    amountOrdered: 0,
+    Decrease: function () {
+      if (this.amountOrdered != 0) {
+        this.amountOrdered -= 1
+      }
+    },
+    addBurger: function () {
+      this.$emit('orderedBurger', {
+        name: this.burger.name,
+        amount: this.amountOrdered
+      }
+      );
+    },
   }
-},
-methods: {
-  Increase: function() {
-   
-    this.amountOrdered += 1;
-  },
-  Decrease: function(){
-    if ( this.amountOrdered!=0){
-    this.amountOrdered -= 1}
-  },
-  addBurger: function () {
-  this.$emit('orderedBurger', { name:   this.burger.name, 
-                                amount: this.amountOrdered 
-                              }
-  );
-},
 }
-  }
-  </script>
+</script>
   
   <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped>
-  /* font-family: 'Times New Roman', Times, serif; */
-  /*img{
+<style scoped>
+/* font-family: 'Times New Roman', Times, serif; */
+/*img{
     width="300" height="300"
   }*/
-  .bilder {
-    max-width: 100%;
-  }
-  </style>
+.bilder {
+  max-width: 100%;
+}
+</style>
   
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,38 +1,35 @@
 <template>
-   
   <header>
-        <h1>Burgers Online</h1>
+    <h1>Burgers Online</h1>
 
-    </header>
-    <main>
-        <section class="Burgers" id="chooseburger">
-            <h2> Select burger </h2>
-            <p1> This is where you select burger </p1>
-            <div class="wrapper">
-              <Burger v-for="burger in burgers"
-              v-bind:burger="burger" 
-              v-bind:key="burger.name"
-              v-on:orderedBurger="addToOrder($event)"/>
-            </div>
+  </header>
+  <main>
+    <section class="Burgers" id="chooseburger">
+      <h2> Select burger </h2>
+      <p1> This is where you select burger </p1>
+      <div class="wrapper">
+        <Burger v-for="burger in burgers" v-bind:burger="burger" v-bind:key="burger.name"
+          v-on:orderedBurger="addToOrder($event)" />
+      </div>
 
-        </section>
+    </section>
 
-        <section id="contact">
-            <h4>
-                Enter customer information
-            </h4>
-            <form>
-              
-                <p>
-                    <label for="firstname">Full name</label><br>
-                    <input type="text" id="firstname" v-model="fn" required="required" placeholder="First and lastname">
-                  
-                </p>
-                <p>
-                    <label for="email">E-mail</label><br>
-                    <input type="email" id="email" v-model="em" required="required" placeholder="E-mail address">
-                </p>
-                <!-- <p>
+    <section id="contact">
+      <h4>
+        Enter customer information
+      </h4>
+      <form>
+
+        <p>
+          <label for="firstname">Full name</label><br>
+          <input type="text" id="firstname" v-model="fn" required="required" placeholder="First and lastname">
+
+        </p>
+        <p>
+          <label for="email">E-mail</label><br>
+          <input type="email" id="email" v-model="em" required="required" placeholder="E-mail address">
+        </p>
+        <!-- <p>
                     <label for="street">Street</label><br>
                     <input type="text" id="street" v-model="st" required="required" placeholder="Street name">
 
@@ -43,61 +40,61 @@
                         maxlength="10">
                 </p> -->
 
-                <div id = "map-container">
-                  
-                  <div id="map" v-on:click="addOrder" >
-                      click here to choose delivery adress
-                 <div id = "dots" v-bind:style="{left: location.x + 'px', top: location.y + 'px'}"  >
-                  <div>
-                    T
-                  </div>
-                 
-                 </div>
-                </div>
-               
-                
-                  
+        <div id="map-container">
+
+          <div id="map" v-on:click="addOrder">
+            click here to choose delivery adress
+            <div id="dots" v-bind:style="{ left: location.x + 'px', top: location.y + 'px' }">
+              <div>
+                T
               </div>
-                <p>
-                    <label for="payment">Payment method</label><br>
-                    <select id="paymentmethod" v-model="pm">
-                        <option>Klarna</option>
-                        <option selected="selected">Swish</option>
-                        <option>Debit-Card</option>
-                        <option>Credit-Card</option>
-                        <option>Svea</option>
 
-                    </select>
-                </p>
-                <p style="display: inline-block">
-                    <label for="gender">Gender</label><br>
-                    <label>
-                        <input type="radio" v-model="gender" value="Female" checked> Female
-                    </label>
-                    <label>
-                        <input type="radio" v-model="gender" value="Male"> Male
-                    </label>
-                    <label>
-                        <input type="radio" v-model="gender" value="unspecified"> Do not wish to provide
-                    </label>
-                </p>
-
-              </form>
-            <button  v-on:click="markDone" class= "button"  type="submit" > Place Order
-                <img src="img/send.png" width="70" height="30">
-            </button> <br>
-            
-
-        </section>
+            </div>
+          </div>
 
 
 
-    </main>
-    <footer>
-        <hr>
-        &COPY; 2023 ALLES BURGERS INC.
+        </div>
+        <p>
+          <label for="payment">Payment method</label><br>
+          <select id="paymentmethod" v-model="pm">
+            <option>Klarna</option>
+            <option selected="selected">Swish</option>
+            <option>Debit-Card</option>
+            <option>Credit-Card</option>
+            <option>Svea</option>
 
-    </footer>
+          </select>
+        </p>
+        <p style="display: inline-block">
+          <label for="gender">Gender</label><br>
+          <label>
+            <input type="radio" v-model="gender" value="Female" checked> Female
+          </label>
+          <label>
+            <input type="radio" v-model="gender" value="Male"> Male
+          </label>
+          <label>
+            <input type="radio" v-model="gender" value="unspecified"> Do not wish to provide
+          </label>
+        </p>
+
+      </form>
+      <button v-on:click="markDone" class="button" type="submit"> Place Order
+        <img src="img/send.png" width="70" height="30">
+      </button> <br>
+
+
+    </section>
+
+
+
+  </main>
+  <footer>
+    <hr>
+    &COPY; 2023 ALLES BURGERS INC.
+
+  </footer>
 </template>
 
 <script>
@@ -114,7 +111,7 @@ const socket = io();
 //   this.gluten = gl;
 //   this.lactose = lac;
 //   this.URL = url;
-  
+
 // }
 // let allBurgers = [{name: "The Mouth-Watering Burger", kCal: 900, url:"img/Mouthwateringburger.jpeg ", gluten: "Gluten-Free", lactose: "Lactose-Free"},
 // {name: "The Devils-Burger", kCal: 100, url:"img/devils burger - stor – medel.jpeg", gluten: "Gluten-free", lactose: "Vegan" },
@@ -131,184 +128,187 @@ export default {
     return {
       burgers: menu,
       fn: '',
-      em:'',
-      st:'',
-      hn:'',
-      pm:'',
-      gender:'',
+      em: '',
+      st: '',
+      hn: '',
+      pm: '',
+      gender: '',
       orderedBurgers: {},
-      location: { x: 0, y: 0}
+      location: { x: 0, y: 0 }
     }
   },
-    
-  
+
+
   methods: {
-    markDone: function (){
-      console.log("hello")
-      console.log(this.location)
-    socket.emit("addOrder", { orderId: this.getOrderNumber(),
-                          details: { x: this.location.x,
-                                     y: this.location.y
-                                    },
-                                    
-                                    informationCustomer: { 
-                                      name : this.fn,
-                                      email: this.em,
-                                      paymentMethod: this.pm,
-                                      Gender: this.gender,
+    markDone: function () {
 
-                                    },
-                                    orderItems: this.orderedBurgers
-                                   
+      socket.emit("addOrder", {
+        orderId: this.getOrderNumber(),
+        details: {
+          x: this.location.x + 5 ,
+          y: this.location.y + 5
+        },
+
+        informationCustomer: {
+          name: this.fn,
+          email: this.em,
+          paymentMethod: this.pm,
+          Gender: this.gender,
+
+        },
+        orderItems: this.orderedBurgers
 
 
-                        }
-           );
-           
- },
+
+      }
+      );
+
+    },
     getOrderNumber: function () {
-      return Math.floor(Math.random()*100000);
+      return Math.floor(Math.random() * 100000);
     },
     addOrder: function (event) {
-      var offset = {x: event.currentTarget.getBoundingClientRect().left,
-                    y: event.currentTarget.getBoundingClientRect().top};
+      var offset = {
+        x: event.currentTarget.getBoundingClientRect().left,
+        y: event.currentTarget.getBoundingClientRect().top
+      };
       // socket.emit("addOrder", { orderId: this.getOrderNumber(),
       //                           details: { x: event.clientX - 10 - offset.x,
       //                                      y: event.clientY - 10 - offset.y },
       //                           orderItems: ["Beans", "Curry"]
       //                         }
       //            );
-                 this.location.x = event.clientX -10 - offset.x;
-                 this.location.y = event.clientY - 10 - offset.y;
+      this.location.x = event.clientX -15 - offset.x;
+      this.location.y = event.clientY - 25 - offset.y;
     },
 
     addToOrder: function (event) {
       this.orderedBurgers[event.name] = event.amount;
 
-    
+    },
 
-  
-},
-   
-       setLocation: function(event){
-        this.location.x= event.clientX - 10 - event.currentTarget.getBoundingClientRect().left;
-        this.location.y= event.clientY - 10 - event.currentTarget.getBoundingClientRect().top;
-       
-       },
+    setLocation: function (event) {
+      this.location.x = event.clientX - 10 - event.currentTarget.getBoundingClientRect().left;
+      this.location.y = event.clientY - 10 - event.currentTarget.getBoundingClientRect().top;
 
-    }
+    },
+
   }
+}
 
 </script>
 
 <style>
- #map-container{
-  
-    overflow: scroll;
-    height: 400px;
-    width: 100%;
-  }
-  #map {
-    background: url("../../public/img/polacks.jpg");
-    background-size: cover;
-    width:1920px;
-    height: 1078px;
-  }
- 
+#map-container {
+
+  overflow: scroll;
+  height: 400px;
+  width: 100%;
+}
+
+#map {
+  background: url("../../public/img/polacks.jpg");
+  background-size: cover;
+  width: 1920px;
+  height: 1078px;
+}
+
 
 body {
-    font-family: 'Times New Roman', Times, serif;
- }
- header {
-    background-image: url("../../public/img/bakgrundhamburger.jpeg");
-    opacity: 0.8;
-    text-align: center;
-    height: 410px;
-    color: black;
-    overflow:hidden;
-    width: 100%;
+  font-family: 'Times New Roman', Times, serif;
+}
 
- }
- 
- h3{
-    padding: 20px;
-    font-size: 20px
- }
+header {
+  background-image: url("../../public/img/bakgrundhamburger.jpeg");
+  opacity: 0.8;
+  text-align: center;
+  height: 410px;
+  color: black;
+  overflow: hidden;
+  width: 100%;
 
- .Burgers {
-    margin-top: 20px;
-    margin-left: 20px;
-    margin-right: 20px;
-    margin-bottom: 20px;
-    border: 15px double whitesmoke;
-    text-align: center;
-    
-    
- }
+}
 
- #chooseburger { 
-    background-color: black;
-    color: whitesmoke;
+h3 {
+  padding: 20px;
+  font-size: 20px
+}
 
- }
+.Burgers {
+  margin-top: 20px;
+  margin-left: 20px;
+  margin-right: 20px;
+  margin-bottom: 20px;
+  border: 15px double whitesmoke;
+  text-align: center;
 
- div {
 
-    margin: 1px 2px 3px 4px;
+}
 
- }
+#chooseburger {
+  background-color: black;
+  color: whitesmoke;
 
- button:hover {
-    background-color: blueviolet;
-    cursor: pointer;
- }
+}
 
- #contact {
-    border: 5px dashed darkgreen;
-    background-color: aquamarine;
-    margin-left: 20px;
-    margin-right: 20px;
+div {
 
- }
- 
- .wrapper {
+  margin: 1px 2px 3px 4px;
+
+}
+
+button:hover {
+  background-color: blueviolet;
+  cursor: pointer;
+}
+
+#contact {
+  border: 5px dashed darkgreen;
+  background-color: aquamarine;
+  margin-left: 20px;
+  margin-right: 20px;
+
+}
+
+.wrapper {
   margin-top: 70px;
   margin-bottom: 30px;
-    display: grid;
-    width:100%;
-    grid-column-gap: 1px;
-    grid-template-columns: repeat(3,1fr);
+  display: grid;
+  width: 100%;
+  grid-column-gap: 1px;
+  grid-template-columns: repeat(3, 1fr);
 }
+
 .bilder {
   width: 300px;
   height: 200px;
 
-} 
+}
+
 .button {
-    margin-top: 50px;
-    margin-bottom: 10px;
-    width: 70; 
-    height: 30;
+  margin-top: 50px;
+  margin-bottom: 10px;
+  width: 70;
+  height: 30;
 
- }
- #dots {
-    position: relative;
-    margin: 0;
-    padding: 0;
-    background-repeat: no-repeat;
-    width:1920px;
-    height: 1078px;
-    cursor: crosshair;
-  }
-  #dots div {
-    position: absolute;
-    background: black;
-    color: white;
-    border-radius: 10px;
-    width:20px;
-    height:20px;
-    text-align: center;
-  }
+}
 
+#dots {
+  position: relative;
+  margin: 0;
+  padding: 0;
+  background-repeat: no-repeat;
+  width: 1920px;
+  height: 1078px;
+  cursor: crosshair;
+}
 
-</style>
+#dots div {
+  position: absolute;
+  background: black;
+  color: white;
+  border-radius: 10px;
+  width: 20px;
+  height: 20px;
+  text-align: center;
+}</style>
